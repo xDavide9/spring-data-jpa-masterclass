63502e11f944aa24ce0423443ea7994b70b788a9
xdavide9 <biancodavidetech@gmail.com>
2024-10-02 09:39:47 +0200

    bootstrap course

---
+++ src/main/java/com/xdavide9/springdatajpamasterclass/SpringDataJpaMasterclassApplication.java
@@ -0,0 +1,13 @@
+package com.xdavide9.springdatajpamasterclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaMasterclassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataJpaMasterclassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xdavide9/springdatajpamasterclass/SpringDataJpaMasterclassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xdavide9.springdatajpamasterclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaMasterclassApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
